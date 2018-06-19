cb4db0d01eeba95b053af0375c4a57862e409a4a
llinero89 <llinero89@gmail.com>
2018-06-18 19:57:57 -0500

    Helloworld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, I'm running from Jenkins...!");
+		System.out.println("Hello, I'm doing some test from Jenkins...!");
     }
 
 }
